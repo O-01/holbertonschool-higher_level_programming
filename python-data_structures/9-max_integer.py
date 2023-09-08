0c137a8b9061116faae5b47404bc7d6a5a10b357
O-01 <sa.an-s@yandex.com>
2023-09-08 16:46:33 -0500

    Fixed missing colon in else-statement - max_integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
-    else
+    else:
         maxval = 0
         for item in range(0, len(my_list)):
             if my_list[item] > maxval:
