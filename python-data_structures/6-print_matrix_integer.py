3b1c9bf5f28f8584838f6a637049475235927c06
O-01 <sa.an-s@yandex.com>
2023-09-08 15:09:21 -0500

    Modified print_matrix_integer for testing

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
+    if matrix == [[]]:
+        print()
+    else:
         for item1 in range(0, len(matrix)):
             for item2 in range(0, len(matrix[item1])):
                 if item2 == len(matrix[item1]) - 1:
