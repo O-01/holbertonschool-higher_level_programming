0ddb06c8b928f1972b74d8d8632967696ecc27bb
O-01 <sa.an-s@yandex.com>
2023-09-08 16:45:26 -0500

    Modified max_integer conditionals

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:
+    if my_list == []:
+        return None
+    else
         maxval = 0
         for item in range(0, len(my_list)):
             if my_list[item] > maxval:
                 maxval = my_list[item]
         return maxval
-    else:
-        return None
