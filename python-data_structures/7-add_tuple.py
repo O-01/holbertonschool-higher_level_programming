aa1670d15ff75e6f76fc2ee922925090fa67227b
O-01 <sa.an-s@yandex.com>
2023-09-08 16:21:32 -0500

    Modified add_tuple addition method

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,10 +4,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = (0, 0)
     elif len(tuple_a) == 1:
         tuple_a = (tuple_a[0], 0)
+
     if len(tuple_b) == 0:
         tuple_b = (0, 0)
     elif len(tuple_b) == 1:
         tuple_b = (tuple_b[0], 0)
 
-    sum_tuple = tuple(map(sum, zip(tuple_a, tuple_b)))
+    sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return sum_tuple
