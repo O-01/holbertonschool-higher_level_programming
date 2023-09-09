9c1c796d575c3d3db3cd29c1f3398c2a072e1ddc
O-01 <sa.an-s@yandex.com>
2023-09-08 21:34:44 -0500

    Added number_keys

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = []
-    for item in sorted(set(set_1)):
-        for comparison in sorted(set(set_2)):
-            if item == comparison:
-                common.append(item)
-    return common
+    #   common = []
+    #   for item in sorted(set(set_1)):
+    #       for comparison in sorted(set(set_2)):
+    #           if item == comparison:
+    #               common.append(item)
+    #   return common
+    return set_1.intersection(set_2)
