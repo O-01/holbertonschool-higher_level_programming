f1afe19117e94ce608d0f2aeb2d433435a16d24a
O-01 <sa.an-s@yandex.com>
2023-09-09 00:15:12 -0500

    Modified if-statement conditional with regard to None - best_score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary:
         high_score = None
         for key, value in a_dictionary.items():
-            if high_score == None or value > high_score:
+            if high_score is None or value > high_score:
                 high_score = value
                 winner = key
         return winner
