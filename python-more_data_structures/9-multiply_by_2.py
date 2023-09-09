8e516b6566cb09e747e0557a227c58e7f9871dac
O-01 <sa.an-s@yandex.com>
2023-09-08 23:06:34 -0500

    Fixed errors per pycodestyle - multiply_by_2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     doubled = {}
     for key, value in a_dictionary.items():
-        doubled.update({key : value * 2})
+        doubled.update({key: value * 2})
     return doubled
