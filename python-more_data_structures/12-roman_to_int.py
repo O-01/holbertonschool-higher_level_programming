e1bb1328f325cf1d6bcfc23c30278910d7c49cf2
O-01 <sa.an-s@yandex.com>
2023-09-10 13:18:15 -0500

    Modified roman_to_int conditionals

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -8,15 +8,20 @@ def roman_to_int(roman_string):
                   'C': 100, 'XC': 90, 'L': 50, 'XL': 40,
                   'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
 
+    add_prev = 0
     for i, n in enumerate(roman_string):
-        if i == 0 and i == len(roman_string) - 1:
-            if n in roman_dict:
-                total += roman_dict[n]
-        elif i < len(roman_string) - 1:
-            if n + roman_string[i + 1] not in roman_dict:
-                total += roman_dict[n]
-            elif n + roman_string[i + 1] in roman_dict:
-                total += roman_dict[n + roman_string[i + 1]]
-        elif i > 0 and roman_string[i - 1] + n not in roman_dict:
-            total += roman_dict[n]
+        if add_prev == 1:
+            total += roman_dict[roman_string[i - 1] + n]
+            add_prev = 0
+            continue
+        else:
+            if i == len(roman_string) - 1:
+                if n in roman_dict:
+                    total += roman_dict[n]
+            elif i < len(roman_string) - 1:
+                if n + roman_string[i + 1] in roman_dict:
+                    add_prev = 1
+                    continue
+                elif n in roman_dict:
+                    total += roman_dict[n]
     return total
