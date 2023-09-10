d5fa43336229f8726e0936c298e5ecd1d32dc368
O-01 <sa.an-s@yandex.com>
2023-09-10 14:42:30 -0500

    Modified weight_average mean variable name for accuracy

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -3,6 +3,6 @@ def weight_average(my_list=[]):
     if my_list == []:
         return 0
     else:
-        score_mean = sum(item[0] * item[1] for item in my_list)
+        mean = sum(item[0] * item[1] for item in my_list)
         weight = sum(item[1] for item in my_list)
-        return score_mean / weight
+        return mean / weight
