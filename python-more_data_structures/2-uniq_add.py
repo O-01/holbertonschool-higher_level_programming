db5a996a33e8f91bb63b6b59aa16912f7af644e1
O-01 <sa.an-s@yandex.com>
2023-09-10 14:52:30 -0500

    Modified uniq_add for fun

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique = []
-    for item in sorted(set(my_list)):
-        unique.append(item)
-    summa = 0
-    for item in range(0, len(unique)):
-        summa += unique[item]
-    return summa
+    return sum(item for item in sorted(set(my_list)))
