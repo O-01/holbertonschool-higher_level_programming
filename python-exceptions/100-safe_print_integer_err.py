d827a756cfe649cd282fa8fee8e05be224727d79
O-01 <sa.an-s@yandex.com>
2023-09-11 15:12:45 -0500

    Modified safe_print_integer_err to print error to stderr

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError) as errr:
-        print("Exception:", errr)
+        print("Exception: {}".format(errr), file = sys.stderr)
         return False
