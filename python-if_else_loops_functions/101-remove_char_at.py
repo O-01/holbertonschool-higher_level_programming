6315b468f73f562ee017c073fc22613a6e316bf7
O-01 <sa.an-s@yandex.com>
2023-09-11 16:06:44 -0500

    Modified remove_char_at for negative index argument condition

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    return str[:n] + str[n + 1:]
+    minus_n = None
+    if n < 0:
+        minus_n = str
+    else:
+        minus_n = str[:n] + str[n + 1:]
+    return minus_n
