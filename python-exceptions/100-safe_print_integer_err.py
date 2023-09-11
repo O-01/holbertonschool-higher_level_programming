8f808d372692e2cc3e606f581b56ebdd52b6919c
O-01 <sa.an-s@yandex.com>
2023-09-11 15:21:07 -0500

    Modified safe_print_integer_err per pycodestyle

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
     except Exception as errr:
-        print("Exception: {}".format(errr), file = sys.stderr)
+        print("Exception: {}".format(errr), file=sys.stderr)
         return False
