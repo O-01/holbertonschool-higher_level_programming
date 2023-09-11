1697fca16625d0ac477e205cd4da6539e60543b2
O-01 <sa.an-s@yandex.com>
2023-09-11 15:08:27 -0500

    Modified safe_print_integer_err

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as verr:
-        print("Exception:", verr)
+    except (TypeError, ValueError) as errr:
+        print("Exception:", errr)
         return False
