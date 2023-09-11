3067b563abdfeb8a7a4e4b7537bf71969e90709c
O-01 <sa.an-s@yandex.com>
2023-09-11 14:41:43 -0500

    Modified list_division per pycodestyle

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-        q_list = []
-        for item in range(0, list_length):
-                try:
-                        quotient = my_list_1[item] / my_list_2[item]
-                except TypeError:
-                        print("wrong type")
-                        quotient = 0
-                except ZeroDivisionError:
-                        print("division by 0")
-                        quotient = 0
-                except IndexError:
-                        print("out of range")
-                        quotient = 0
-                finally:
-                        q_list.append(quotient)
-        return q_list
+    q_list = []
+    for item in range(0, list_length):
+        try:
+            quotient = my_list_1[item] / my_list_2[item]
+        except TypeError:
+            print("wrong type")
+            quotient = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            quotient = 0
+        except IndexError:
+            print("out of range")
+            quotient = 0
+        finally:
+            q_list.append(quotient)
+    return q_list
