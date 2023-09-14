5f815c5bf3a08f747ad2960c527b33c502307d5e
O-01 <sa.an-s@yandex.com>
2023-09-14 15:19:47 -0500

    Modified 5-square - added doclines to each method, modified output

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -5,6 +5,7 @@
 class Square:
     """ class instantiated by __size upon meeting type/value conditions """
     def __init__(self, size=0):
+        """ initialize Square class """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -14,21 +15,27 @@ class Square:
 
     @property
     def size(self):
+        """ gets size attribute of Square """
         return self.__size
 
     @size.setter
-    def size(self, size=0):
-        if not isinstance(size, int):
+    def size(self, value=0):
+        """ sets size attribute of Square to value """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
+        """ calculates area of Square """
         return self.__size ** 2
 
     def my_print(self):
+        """ prints visual representation of Square using # character """
+        if self.__size == 0:
+            print()
         for row in range(self.__size):
             for col in range(self.__size):
                 print("#", end="")
