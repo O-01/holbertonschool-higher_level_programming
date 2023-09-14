8dc58d79280dbac55f3af18226f3b1a9b760cf2a
O-01 <sa.an-s@yandex.com>
2023-09-14 14:18:50 -0500

    Added square (instantiated)

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" defines a square by __size with instantiation """
+
+
+class Square:
+    """ Square class instantiated by __size """
+    def __init__(self, __size="0"):
+        self.__size = __size
