cbaf8dafb3ebf275df40212802e3a95ce7ba8bc8
O-01 <sa.an-s@yandex.com>
2023-09-14 14:54:31 -0500

    Added square (getter & setter)

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" added functionality that allows private attribute access & update """
+
+
+class Square:
+    """ class instantiated by __size upon meeting type/value conditions """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
