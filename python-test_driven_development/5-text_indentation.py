4ea6f888cd1f7e5f728fff0fe435c8790b2d975b
O-01 <sa.an-s@yandex.com>
2023-09-15 18:56:26 -0500

    Added text_indentation - corresponding doctest file in tests directory

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" prints a given text with 2 line breaks after ./?/: delimiters reached """
+
+
+def text_indentation(text):
+    """ prints a given text with 2 line breaks after ./?/: delimiters reached -
+    text must be a string without spaces at either the beginning or end of each
+    printed line """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for idx, char in enumerate(text):
+        if idx > 0 and text[idx - 1] in ".?:":
+            if char == " ":
+                char = ""
+            print("\n")
+        print(char, end="")
