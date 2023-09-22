e79a2f1aadf63adf5767f4d793507465f9ea1105
O-01 <sa.an-s@yandex.com>
2023-09-22 12:25:00 -0500

    Modified is_same_class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,4 +4,7 @@
 
 def is_same_class(obj, a_class):
     """ verifies if input object is of specified input class """
-    return isinstance(obj, a_class)
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
