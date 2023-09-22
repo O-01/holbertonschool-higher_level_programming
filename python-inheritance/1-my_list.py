ff01d1a297b2ed28f300a9d54c4b1bde992d4464
O-01 <sa.an-s@yandex.com>
2023-09-22 15:15:08 -0500

    Modified my_list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,10 +4,6 @@
 
 class MyList(list):
     """ MyList class inherited from list - sorts input """
-    def __init__(self):
-        """ instantiates MyList object """
-        super().__init__()
-
     def print_sorted(self):
         """ prints sorted list """
         print(sorted(self))
