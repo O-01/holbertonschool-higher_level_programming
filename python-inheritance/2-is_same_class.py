b0dab3d3edc1fdb94d48429e4bb8820ad0e03034
O-01 <sa.an-s@yandex.com>
2023-09-22 12:21:12 -0500

    Added is_same_class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" verifies if input object is of specified input class """
+
+
+def is_same_class(obj, a_class):
+    """ verifies if input object is of specified input class """
+    return isinstance(obj, a_class)
