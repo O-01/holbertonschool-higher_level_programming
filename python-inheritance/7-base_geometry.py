f488fa39def8415f7917d62322d476e6eb7a6972
O-01 <sa.an-s@yandex.com>
2023-09-23 01:44:44 -0500

    Modified 7-base_geometry - added comments to class methods

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,9 +5,11 @@
 class BaseGeometry:
     """ introduce area method that raises exception """
     def area(self):
+        """ raises exception due to area not yet implemented """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """ verifies that input value for name is integer above zero """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
