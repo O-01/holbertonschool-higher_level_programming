ba985419cdcece4e62b84129ad7865b6289cc451
O-01 <sa.an-s@yandex.com>
2023-09-25 14:30:14 -0500

    Modified read_line

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ reads UTF8 formatted text file, printing to stdout """
     with open(filename, 'r', encoding="utf8") as fillle:
         for line in fillle:
-            print(fillle.readline())
+            print(line, end="")
