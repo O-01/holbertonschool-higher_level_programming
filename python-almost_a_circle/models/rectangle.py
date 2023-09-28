ab25cb199e1019533bd90758b7623457a8d8194f
O-01 <sa.an-s@yandex.com>
2023-09-28 03:43:16 -0500

    Modified rectangle.py (2) per pycodestyle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """ defines Rectangle subclass of Base """
-    def  __init__(self, width, height, x=0, y=0, id=None):
+    def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
         self.x = x
