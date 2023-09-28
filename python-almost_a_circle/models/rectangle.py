a77b4387d67c055834a7d319d9ad38593b6ad581
O-01 <sa.an-s@yandex.com>
2023-09-28 15:22:08 -0500

    Added kwargs update to [9]rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,19 +73,32 @@ class Rectangle(Base):
                 print("#", end="")
             print()
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ updates attributes of Rectangle object """
-        for iteration, item in enumerate(args):
-            if iteration == 0:
-                self.id = item
-            if iteration == 1:
-                self.width = item
-            if iteration == 2:
-                self.height = item
-            if iteration == 3:
-                self.x = item
-            if iteration == 4:
-                self.y = item
+        if args and len(args) > 0:
+            for index, item in enumerate(args):
+                if index == 0:
+                    self.id = item
+                if index == 1:
+                    self.width = item
+                if index == 2:
+                    self.height = item
+                if index == 3:
+                    self.x = item
+                if index == 4:
+                    self.y = item
+        elif kwargs and len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.width = value
+                if key == "height":
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
 
     def __str__(self):
         """ overrides parent class __str__ method """
