fd069bcb1b4a51336f18ae5063faa403137cf749
O-01 <sa.an-s@yandex.com>
2023-10-20 00:22:23 -0500

    Added 14-average, modified certain formatting in {7..9}

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- computes score average of all records in table second_table of db hbtn_0c_0
+-- new column named average
+SELECT AVG(score) as average FROM second_table;
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- displays no. of records with id 89 in table first_table of db hbtn_0c_0
-SELECT COUNT(id) FROM first_table WHERE `id` = '89';
+SELECT COUNT(id) FROM first_table WHERE id = 89;
