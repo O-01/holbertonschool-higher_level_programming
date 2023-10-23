9d98f49b7968ab68625b47c8de1a6d26fb65809a
O-01 <sa.an-s@yandex.com>
2023-10-23 10:47:25 -0500

    Added 1-create_user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates MySQL server user user_0d_1 if not already existing
+-- all privileges
+-- password: user_0d_1_pwd
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1@localhost;
