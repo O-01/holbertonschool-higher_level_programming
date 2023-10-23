e8168d0d7102fe0b9120599fc33ce8f500a806ff
O-01 <sa.an-s@yandex.com>
2023-10-23 13:16:21 -0500

    Added 12-no_genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- lists all shows contained in the database hbtn_0d_tvshows
+-- sorted in ascending order by tv_shows.title, tv_show_genres.genre_id
+-- if show has no genre, display NULL
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
