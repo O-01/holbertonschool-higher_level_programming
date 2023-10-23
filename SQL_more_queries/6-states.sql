37d5339ff01167f471e0878c51257073747e5a11
O-01 <sa.an-s@yandex.com>
2023-10-23 11:20:15 -0500

    Corrected 6-states to have correct syntax

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,9 @@
 -- creates database hbtn_0d_usa if not already existing
 -- creates the table states on hbtn_0d_usa if not already there
--- id INT DEFAULT NOT NULL UNIQUE PRIMARY KEY, AUTO GENERATED
+-- id INT NOT NULL UNIQUE PRIMARY KEY, AUTO GENERATED
 -- name VARCHAR(256) NOT NULL
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT UNIQUE AUTO_INCREMENT DEFAULT NOT NULL PRIMARY KEY,
-    name VARCHAR(256) DEFAULT NOT NULL
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
 );
