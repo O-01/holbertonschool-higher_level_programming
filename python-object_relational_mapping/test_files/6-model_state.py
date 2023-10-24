84dcdd9c77055e405b9b18058d81f490b60dc936
O-01 <sa.an-s@yandex.com>
2023-10-24 15:31:58 -0500

    Modified model_state

--- python-object_relational_mapping/test_files/6-model_state.py
+++ python-object_relational_mapping/test_files/6-model_state.py
@@ -3,12 +3,18 @@
 """
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+        'mariadb+mariadbconnector://{}:{}@127.0.0.1:3306/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True)
     )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State)
     Base.metadata.create_all(engine)
+    session.close()
