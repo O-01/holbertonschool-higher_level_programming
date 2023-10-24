5c44eebc37c7b02d356bd7b1aae02e407206df88
O-01 <sa.an-s@yandex.com>
2023-10-24 16:09:06 -0500

    Modified 7 again

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,11 +14,11 @@ Your code should not be executed when imported
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
+from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
     )
     Session = sessionmaker(bind=engine)
