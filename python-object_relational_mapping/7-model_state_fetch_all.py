929ca11a7151b0e8687ee43ff51a90554869dbe7
O-01 <sa.an-s@yandex.com>
2023-10-24 16:16:02 -0500

    Restructure 7-model_state_fetch_all - import correct python3

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Write a script that lists all State objects from the database hbtn_0e_6_usa
 Your script should take 3 arguments: mysql username, mysql password and
@@ -11,20 +11,19 @@ Results must be sorted in ascending order by states.id
 The results must be displayed as they are in the example below
 Your code should not be executed when imported
 """
-import sys
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}',
+        pool_pre_ping=True
     )
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-    [print("{}: {}".format(state.id, state.name)) for state in states]
-
-    session.close()
+    for state in session.query(State).order_by(State.id).all():
+        print(f"{state.id}: {state.name}")
