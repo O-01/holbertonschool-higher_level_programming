380c91d22bbc239557d4915d185b13c3c3d31d7a
O-01 <sa.an-s@yandex.com>
2023-11-22 14:55:47 -0600

    Added 5-to_integer & 6-multi_languages_loop

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const intArg = parseInt(process.argv[2]);
+if (intArg) {
+  console.log(`My number: ${intArg}`);
+} else {
+  console.log('Not a number');
+}
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const langStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let iter = 0; iter !== langStrings.length; iter++) {
+  console.log(langStrings[iter]);
+}
