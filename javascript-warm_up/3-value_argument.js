a833a01257aec158334d5c8b88f11a5cc9397dbd
O-01 <sa.an-s@yandex.com>
2023-11-22 14:25:10 -0600

    Modified 3-value_argument & 4-concat per formatting restrictions

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const argVector = process.argv;
-if (argVector.length === 2) {
-  console.log('No argument');
+if (process.argv[2]) {
+  console.log(process.argv[2]);
 } else {
-  console.log(argVector[2]);
+  console.log('No argument');
 }
--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const argVector = process.argv;
-if (argVector.length === 2) {
-  console.log('No argument');
-} else {
-  console.log(`${argVector[2]} is ${argVector[3]}`);
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
