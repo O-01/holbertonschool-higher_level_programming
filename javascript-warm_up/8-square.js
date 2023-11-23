412e43d7e6862f97d5f79688f3a0895b63085ca6
O-01 <sa.an-s@yandex.com>
2023-11-23 16:40:33 -0600

    Moved test files to reference folder, simplified 8-square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const intArg = parseInt(process.argv[2]);
-if (intArg) {
-  for (let row = 0; row < intArg; row++) {
-    for (let col = 0; col < intArg; col++) process.stdout.write('X');
-    console.log();
-  }
-} else console.log('Missing size');
+const size = parseInt(process.argv[2]);
+if (size) for (let row = 0; row < size; row++) console.log('X'.repeat(size));
+else console.log('Missing size');
