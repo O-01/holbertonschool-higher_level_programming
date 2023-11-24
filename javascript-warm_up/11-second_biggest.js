2d115875ac1b0c596d129bcdd06e45b77fa9894a
O-01 <sa.an-s@yandex.com>
2023-11-23 18:34:29 -0600

    Modified 11-second_biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const inputs = process.argv.slice(2).sort((x, y) => x - y);
-inputs[1] ? console.log(inputs[inputs.length - 2]) : console.log(0);
+const inputs = process.argv.slice(2);
+inputs[1] ? console.log(inputs.sort((x, y) => x - y)[inputs.length - 2]) : console.log(0);
