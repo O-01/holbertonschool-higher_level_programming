43247bcea3c6533a27b68e33fa335b07795bb0da
O-01 <sa.an-s@yandex.com>
2023-11-23 18:23:05 -0600

    Suitably modified 11-second_biggest to be compatible with older JavaScript versions

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const inputs = process.argv.slice(2).sort((x, y) => x - y);
-inputs[1] ? console.log(inputs.at(-2)) : console.log(0);
+inputs[1] ? console.log(inputs[inputs.length - 2]) : console.log(0);
