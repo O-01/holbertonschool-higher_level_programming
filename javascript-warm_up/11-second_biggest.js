54d4bf065773c0bccf90b0874df1e29bc028b58d
O-01 <sa.an-s@yandex.com>
2023-11-28 15:28:36 -0600

    Further simplified sorting mechanism in 11-second_biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const inputs = process.argv.slice(2);
-inputs[1] ? console.log(inputs.sort((x, y) => x - y)[inputs.length - 2]) : console.log(0);
+inputs[1] ? console.log(inputs.sort((x, y) => y - x)[1]) : console.log(0);
