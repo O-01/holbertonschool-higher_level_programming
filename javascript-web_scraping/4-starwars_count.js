baf4bff8ec0c6d24d0dc61d1bf1704bc64f1190c
O-01 <sa.an-s@yandex.com>
2023-11-30 06:23:49 -0600

    Modified 4-starwars_count to not use <array>.flat() due incompatibility with older ES versions

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,9 @@ request.get(process.argv[2], { timeout: 3000 }, (err, response, body) => {
   if (err) console.error(err);
   else if (response.statusCode === 404) console.log('404 NOT FOUND');
   else if (body) {
-    const wedge = JSON.parse(body).results.map((film) => film.characters.filter((char) => char.match(/.\/18\//g))).flat();
-    console.log(wedge);
-    console.log(wedge.length);
+    const characters = JSON.parse(body).results.map((film) => film.characters)
+    const wedges = characters.reduce((out, char) => out.concat(char.filter((entry) => entry.match(/.\/18\//g))), [])
+    console.log(wedges.length);
   }
 });
 
