84b6477e10d56b1597399982da38f517561f1471
O-01 <sa.an-s@yandex.com>
2023-11-30 03:30:02 -0600

    Modified 2-statuscode per semistandard formatting guidelines & reduced GET request timeout to 1 second (1000 ms)

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2], {timeout: 2000}, (err, response) => {
+request.get(process.argv[2], { timeout: 1000 }, (err, response) => {
   if (err) console.error(err);
   else if (response) console.log(`code: ${response.statusCode}`);
 });
