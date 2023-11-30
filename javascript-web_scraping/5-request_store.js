146dc03507ef88ee4d7ed98b42ba79024893cbdb
O-01 <sa.an-s@yandex.com>
2023-11-30 07:13:43 -0600

    Modified 5-request_store to record even blank pages

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,8 +3,8 @@ const request = require('request');
 request.get(process.argv[2], { timeout: 3000 }, (err, response, body) => {
   if (err) console.error(err);
   else if (response.statusCode === 404) console.log('404 NOT FOUND');
-  else if (body) {
+  else {
     const fs = require('fs');
-    fs.writeFile(process.argv[3], body, 'utf8', (err) => { if (err) throw err; });
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => { if (err) throw err });
   }
 });
