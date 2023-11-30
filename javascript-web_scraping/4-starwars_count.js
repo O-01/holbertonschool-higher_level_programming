03ebb3676d7f25b23c5d9f12335e88c9c8edad74
O-01 <sa.an-s@yandex.com>
2023-11-30 06:27:33 -0600

    Modified 4-starwars_count per semistandard formatting guidelines

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,16 +4,8 @@ request.get(process.argv[2], { timeout: 3000 }, (err, response, body) => {
   if (err) console.error(err);
   else if (response.statusCode === 404) console.log('404 NOT FOUND');
   else if (body) {
-    const characters = JSON.parse(body).results.map((film) => film.characters)
-    const wedges = characters.reduce((out, char) => out.concat(char.filter((entry) => entry.match(/.\/18\//g))), [])
-    console.log(wedges.length);
+    const chars = JSON.parse(body).results.map((film) => film.characters);
+    const wedge = chars.reduce((out, char) => out.concat(char.filter((entry) => entry.match(/.\/18\//g))), []);
+    console.log(wedge.length);
   }
 });
-
-// const results = JSON.parse(body).results
-// results.forEach((film) => {
-//   film.characters.forEach((character) => {
-//     if (character.match(/.\/18\//g)) console.log('Found');
-//   })
-// });
-// console.log(results);
