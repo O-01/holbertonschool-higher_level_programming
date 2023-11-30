dfd1fb1d1bbcde24dcf3c47d55b3cd0ce0d19372
O-01 <sa.an-s@yandex.com>
2023-11-30 06:43:51 -0600

    Added 5-request_store, moved test files to their own folder, modified write encoding in 1-writeme

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
   if (err) console.error(err);
 });
---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], { timeout: 3000 }, (err, response, body) => {
+  if (err) console.error(err);
+  else if (response.statusCode === 404) console.log('404 NOT FOUND');
+  else if (body) {
+    const fs = require('fs');
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => { if (err) throw err; });
+  }
+});
