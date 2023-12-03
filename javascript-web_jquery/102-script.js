13e0340cb45597bdbb00490138875f22d61b3dc6
O-01 <sa.an-s@yandex.com>
2023-12-03 02:34:29 -0600

    Added note in comments of 102-script & 103-script

--- javascript-web_jquery/102-script.js
+++ javascript-web_jquery/102-script.js
@@ -2,8 +2,10 @@ $(() => $('input#btn_translate').click(() => {
   $.get(`https://hellosalut.stefanbohacek.dev/?lang=${$('input#language_code').val()}`, (dump) => $('div#hello').text(dump.hello));
 }));
 
+// NOTE: https://www.fourtonfish.com/hellosalut/hello/ NO LONGER ACTIVE - REDIRECTS TO https://hellosalut.stefanbohacek.dev/hello
+
 // Write a JavaScript script that fetches and prints how to say “Hello” depending on the language
-// - You should use this API service: https://www.fourtonfish.com/hellosalut/hello/ (redirects to https://hellosalut.stefanbohacek.dev/hello)
+// - You should use this API service: https://www.fourtonfish.com/hellosalut/hello/
 // - The language code will be the value entered in the tag INPUT#language_code (ex: es, fr, en etc.)
 // - The translation must be fetched when the user clicks on INPUT#btn_translate
 // - The translation of “Hello” must be displayed in the HTML tag DIV#hello
--- javascript-web_jquery/103-script.js
+++ javascript-web_jquery/103-script.js
@@ -4,8 +4,10 @@ $(() => {
   $('input#btn_translate').click(() => sayHello());
 });
 
+// NOTE: https://www.fourtonfish.com/hellosalut/hello/ NO LONGER ACTIVE - REDIRECTS TO https://hellosalut.stefanbohacek.dev/hello
+
 // Write a JavaScript script that fetches and prints how to say “Hello” depending on the language
-// - You should use this API service: https://www.fourtonfish.com/hellosalut/hello/ (redirects to https://hellosalut.stefanbohacek.dev/hello)
+// - You should use this API service: https://www.fourtonfish.com/hellosalut/hello/
 // - The language code will be the value entered in the tag INPUT#language_code (ex: es, fr, en etc.)
 // - The translation must be fetched when the user clicks on INPUT#btn_translate OR presses ENTER when the focus is on INPUT#language_code
 // - The translation of “Hello” must be displayed in the HTML tag DIV#hello
