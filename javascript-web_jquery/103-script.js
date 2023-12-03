d037c60438397d4c623ec1d15d7f01ba9733bb35
O-01 <sa.an-s@yandex.com>
2023-12-03 02:22:02 -0600

    Added 103-script, more aesthetic modifications to 102-script

--- javascript-web_jquery/103-script.js
+++ javascript-web_jquery/103-script.js
@@ -1,11 +1,13 @@
-$(() => $('input#btn_translate').click(() => {
-  $.get(`https://hellosalut.stefanbohacek.dev/?lang=${$('input#language_code').val()}`, (dump) => $('div#hello').text(dump.hello));
-}));
+$(() => {
+  const sayHello = () => $.get(`https://hellosalut.stefanbohacek.dev/?lang=${$('input#language_code').val()}`, (dump) => $('div#hello').text(dump.hello));
+  $('input#language_code').on('keypress', (event) => { if (event.which === 13) sayHello(); });
+  $('input#btn_translate').click(() => sayHello());
+});
 
 // Write a JavaScript script that fetches and prints how to say “Hello” depending on the language
 // - You should use this API service: https://www.fourtonfish.com/hellosalut/hello/ (redirects to https://hellosalut.stefanbohacek.dev/hello)
 // - The language code will be the value entered in the tag INPUT#language_code (ex: es, fr, en etc.)
-// - The translation must be fetched when the user clicks on INPUT#btn_translate
+// - The translation must be fetched when the user clicks on INPUT#btn_translate OR presses ENTER when the focus is on INPUT#language_code
 // - The translation of “Hello” must be displayed in the HTML tag DIV#hello
 // - You can’t use document.querySelector to select the HTML tag
 // - You must use the JQuery API
