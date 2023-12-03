1c07e7c3bd4593240ffe4ff8f8387384c81f776e
O-01 <sa.an-s@yandex.com>
2023-12-02 22:08:43 -0600

    Added {0..3}.script, added all test html files

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+$('div#red_header').on('click', () => $('header').css({ color: '#FF0000' }));
+
+// Write a JavaScript script that updates the text color of the <header>
+// element to red (#FF0000) when the user clicks on the tag DIV#red_header:
+// - You can’t use document.querySelector to select the HTML tag
+// - You must use the JQuery API
---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+$('div#red_header').on('click', () => $('header').addClass('red'));
+
+// Write a JavaScript script that adds the class red to the <header> element when
+// the user clicks on the tag DIV#red_header
+// - You can’t use document.querySelector to select the HTML tag
+// - You must use the JQuery API
