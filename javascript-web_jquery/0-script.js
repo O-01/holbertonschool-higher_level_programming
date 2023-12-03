e2cddee92b004128efed3774da388f4f6f3a959e
O-01 <sa.an-s@yandex.com>
2023-12-03 00:46:54 -0600

    Added 100-script & test html files for advanced tasks, modified 0-script to use .setProperty() style declaration

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -1,4 +1,4 @@
-document.querySelector('header').style.color = '#FF0000';
+document.querySelector('header').style.setProperty('color', '#FF0000');
 
 // Write a JavaScript script that updates the text color of the <header> element to red (#FF0000):
 // - You must use document.querySelector to select the HTML tag
---
+++ javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => document.querySelector('header').style.setProperty('color', '#FF0000'));
+
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000):
+// - You must use document.querySelector to select the HTML tag
+// - You can’t use the jQuery API
+// - Note: Your script must be imported from the <head> tag, not at the end of the HTML
