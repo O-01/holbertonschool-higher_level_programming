48ceb4516d45929feac6a3c486ccc9a41809e1dd
O-01 <sa.an-s@yandex.com>
2023-12-02 22:55:29 -0600

    Added 7-script

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (char) => $('div#character').text(char.name));
+
+// Write a JavaScript script that fetches the character name from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json
+// - The name must be displayed in the HTML tag DIV#character
+// - You can’t use document.querySelector to select the HTML tag
+// - You must use the JQuery API
