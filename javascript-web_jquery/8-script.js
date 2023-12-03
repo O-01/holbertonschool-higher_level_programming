3c89dbc6213afa7ea30e3e8838371823dff48ebb
O-01 <sa.an-s@yandex.com>
2023-12-03 00:16:00 -0600

    After all, why not? Why shouldn't I keep it?

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,7 +1,4 @@
-$.get(
-  'https://swapi-api.hbtn.io/api/films/?format=json',
-  (dump) => $('ul#list_movies').append(dump.results.map((film) => `<li>${film.title}</li>`))
-);
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', (dump) => $('ul#list_movies').append(dump.results.map((film) => `<li>${film.title}</li>`)));
 
 // Write a JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.hbtn.io/api/films/?format=json
 // - All movie titles must be list in the HTML tag UL#list_movies
