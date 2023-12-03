bbe9461411d80d9e9e8368a0d5df32e1b5b6043e
O-01 <sa.an-s@yandex.com>
2023-12-02 22:32:07 -0600

    Added 4-script, modified {2,3}-script with simplified click event trigger

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -1,4 +1,4 @@
-$('div#red_header').on('click', () => $('header').css({ color: '#FF0000' }));
+$('div#red_header').click(() => $('header').css({ color: '#FF0000' }));
 
 // Write a JavaScript script that updates the text color of the <header>
 // element to red (#FF0000) when the user clicks on the tag DIV#red_header:
--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -1,4 +1,4 @@
-$('div#red_header').on('click', () => $('header').addClass('red'));
+$('div#red_header').click(() => $('header').addClass('red'));
 
 // Write a JavaScript script that adds the class red to the <header> element when
 // the user clicks on the tag DIV#red_header
